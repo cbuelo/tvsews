85c51831178aa03a7830746fa5815a3babc2b70d
cbuelo <cbuelo10@gmail.com>
2021-01-29 21:43:10 -0600

    added plotting function for QD results

--- R/quickest_detection.R
+++ R/quickest_detection.R
@@ -165,3 +165,63 @@ run_qd <- function(rolling_window_stats, var_cols, widths = c(21), stats_to_qd =
   }
   return(rolling_window_stats_forCalcs)
 }
+
+
+
+
+#' title
+#'
+#' @param rolling_window_stats data frame of rolling window statistics that is output from \code{\link{calc_rolling_stats}}
+#' @param qd_stats data frame of quickest detection results, output from \code{\link{run_qd}}
+#' @param var_col character vector, variable names that are columns of *data* containing time series to calculate rolling window stats on
+#' @param stats statistics to plot, default and options are c("mean", "SD", "Ar1")
+#' @param title title to put on the plot
+#' @param widths numeric vector, rolling window width(s) to be used, default is c(21)
+#' @param plot_bloom_lines TRUE or FALSE (default), should lines for the bloom dates be plotted?
+#' @param bloom_dates if plot_bloom_lines = TRUE, this data frame is used to determine where
+#'
+#' @return a ggplot object
+#' @import ggplot2
+#' @export
+
+#'
+#' @examples
+#' use_vars = c("Manual_Chl", "BGA_HYLB", "DO_Sat", "pH")
+#' rw_stats = calc_rolling_stats(ts_data, var_cols=use_vars)
+#' qd_stats = run_qd(rw_stats, var_cols=use_vars)
+#' plot_qd(rw_stats, qd_stats, use_vars[1], stats = c("SD", "Ar1"))
+plot_qd <- function(rolling_window_stats, qd_stats, var_col, stats=c("mean", "SD", "Ar1"), title="R.W. width = 21", widths=c(21), plot_bloom_lines = FALSE, bloom_dates){
+  # transform QD df to make it easy to add points
+  alarmCols = paste0(var_col, c("_Ar1_Alarm", "_SD_Alarm"))
+  inds_anyAlarm = apply(qd_stats[, alarmCols], MARGIN = 1, FUN=function(x) !all(is.na(x)) & any(x==1, na.rm=TRUE))
+  QD_long0 = as.data.frame(
+    tidyr::pivot_longer(qd_stats[inds_anyAlarm & qd_stats$Width %in% widths, ], cols = dplyr::starts_with(var_col), names_prefix = var_col, names_to="Stat", values_to = var_col)
+    )
+  QD_long0[, "Stat"] = sub(pattern="_", replacement="", x=QD_long0[, "Stat"])
+  QD_long = QD_long0[QD_long0[, "Stat"] %in% c(paste0(stats, "_Exp"), paste0(stats, "_Alarm")), c("Year", "Lake", "DOYtrunc", "Width", "Stat", var_col)]
+  Alarms = QD_long[grep(pattern="Alarm", QD_long$Stat) & QD_long[, var_col] == 1, ]
+  Alarms[, "Stat"] = gsub(pattern="_Alarm", replacement="", x=Alarms[, "Stat"])
+  colnames(Alarms)[colnames(Alarms) == var_col] = "Alarm"
+  statsAll = QD_long[QD_long[, "Stat"] %in% paste0(stats, "_Exp"), ]
+  statsAll[, "Stat"] = gsub(pattern="_Exp", replacement="", x=statsAll[, "Stat"])
+  # print(head(Alarms))
+
+  statsAlarms = merge(Alarms, statsAll)
+  statsAlarms$Stat = factor(statsAlarms$Stat, levels=stats)
+  # add the points
+  p = ggplot(rolling_window_stats[rolling_window_stats$Stat %in% stats & rolling_window_stats$Width %in% widths, ], aes(x=DOYtrunc, y=get(var_col), color=Lake)) +
+    facet_grid(cols=vars(Year), rows=vars(Stat), scales="free_y") + geom_line() +
+    geom_point(data=statsAlarms, aes(x=DOYtrunc, y=get(var_col), color=Lake)) +
+    theme_bw(base_size=16) + labs(y=var_col, x="Day of Year")
+  if(length(unique(rolling_window_stats$Lake)) == 2){
+    p = p + scale_color_manual(values=c("dodgerblue", "orangered3"))
+  }
+  if(plot_bloom_lines){
+    p = p + geom_vline(data = bloom_dates, aes(xintercept=bloomStartDOY, color=Lake), linetype="dashed") +
+      geom_vline(data = bloom_dates, aes(xintercept=fertStartDOY), color="black", linetype="dashed")
+  }
+  if(!is.na(title)){
+    p = p + ggtitle(title)
+  }
+  return(p)
+}
